85e84aff5833aef06ab8703dadfc9a75bac0a318
unknown <CS024183@WIN1551669.northamerica.cerner.net>
2015-07-07 10:57:35 -0400

    update json

--- server.js
+++ server.js
@@ -11,13 +11,12 @@ var T = new Twit({
 
 var symbol = '$MBLY';
 var fs = require('fs');
-var fromDate = '2015-01-01';
+var fromDate = '2015-06-28';
 var toDate = '2015-05-31';
 
 yahooFinance.historical({
   symbol: symbol.substr(1),
-  from: fromDate,
-  to: toDate
+  from: fromDate
 }, function (err, quotes, url, symbol) {
 	fs.writeFile('./quotes.json', JSON.stringify(quotes), function(err) {
 	    if(err) {
@@ -35,9 +34,19 @@ var maxId = 0;
 var qString = symbol+' since:'+fromDate;
 console.log(qString);
 
+
+function printTweet(statuses){
+	for(var i = 0; i < statuses.length; i++){
+		console.log(statuses[i]['created_at']);
+		console.log(statuses[i].text);
+		console.log('----------------------');
+	}
+	console.log('************************');
+}
+
 function getTweets(maxId){
 	T.get('search/tweets', { q: qString, lang: 'en', count: 100, max_id: maxId}, function(err, data, response) {
-	    if(!data){
+	    if(data === undefined || data.statuses.length===0){
 			fs.writeFile('./tweets.json', JSON.stringify(tweets), function(err) {
 			    if(err) {
 			        return console.log(err);
@@ -46,19 +55,19 @@ function getTweets(maxId){
 			}); 	    
 		}
 	    else{
-	    	tweets.statuses.push(data.statuses);
-	    	console.log(JSON.stringify(data.statuses));
-	    	maxId = data.statuses[0].id;
+	    	tweets.statuses = tweets.statuses.concat(data.statuses);
+	    	printTweet(data.statuses);
+	    	maxId = data.statuses[data.statuses.length-1].id;
 	    	getTweets(maxId);
 	    }
 	})
 }
 
 T.get('search/tweets', { q: qString, lang: 'en', count: 100}, function(err, data, response) {
-	if(data){
-		tweets.statuses.push(data.statuses);
-	    console.log(JSON.stringify(data.statuses));
-		maxId = data.statuses[0].id;
+	if(data!==undefined && data.statuses.length>0){
+		tweets.statuses = tweets.statuses.concat(data.statuses);
+	   	printTweet(data.statuses);
+		maxId = data.statuses[data.statuses.length-1].id;
 		getTweets(maxId);
 	}
 	else{
